e86f92eebfb26859c6782c195aa1fae2f50c133f
Sander Versluys <sversluys@reference.be>
2013-06-12 12:24:22 +0200

    Change fragment shader and show some colors

--- tut1/gltest/main.cpp
+++ tut1/gltest/main.cpp
@@ -38,9 +38,9 @@ int init_resources(void)
     const char *fs_source =
     "#version 120           \n"
     "void main(void) {        "
-    "  gl_FragColor[0] = 0.0; "
-    "  gl_FragColor[1] = 0.0; "
-    "  gl_FragColor[2] = 1.0; "
+    "  gl_FragColor[0] = gl_FragCoord.x/640.0;"
+    "  gl_FragColor[1] = gl_FragCoord.y/480.0;"
+    "  gl_FragColor[2] = 0.5; "
     "}";
     glShaderSource(fs, 1, &fs_source, NULL);
     glCompileShader(fs);
